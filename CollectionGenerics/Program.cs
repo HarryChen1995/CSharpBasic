c71e1cc3752b4932cd9138055f4363c49541a860
Hanlin Chen <harrychen1995@yahoo.com>
2021-05-24 14:04:25 -0400

    Collection Generics added

---
+++ CollectionGenerics/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace CollectionGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> stringArr = new List<string>();
+            stringArr.Add("fist");
+            stringArr.AddRange(new List<String> {"Second", "Third"});
+            foreach(string s in stringArr)
+            Console.WriteLine(s);
+
+            Dictionary<string, int> hashtable = new Dictionary<string, int>();
+            hashtable["tom"] = 12;
+            hashtable["tom"] += 1;
+            Console.WriteLine(hashtable.ContainsKey("asd"));
+            int value;
+            if (hashtable.TryGetValue("tom", out value)){
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
